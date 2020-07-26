d6d69bc859cdc03ce03c84f80ed4a853a943a635
JSR33 <joao.norbain@gmail.com>
2020-07-26 19:41:11 +0100

    app

---
+++ coin-converter/src/Components/Conversor.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+import './Conversor.css'
+
+
+export default class Conversor extends Component{
+    
+    constructor(props){
+        super(props);
+
+        this.state = {
+            moedaA_valor : "",
+            moedaB_valor : 0
+        }
+
+        this.converter = this.converter.bind(this);
+    }
+
+    converter(){
+        let moedaB_valor = (parseFloat(this.state.moedaA_valor)*2).toFixed(2);
+            this.setState({moedaB_valor})
+    }
+    
+    render(){
+        return(
+            <div className="conversor">
+                <h2>{this.props.moedaA} para {this.props.moedaB}</h2>                 
+                <input type="text" onChange={(event)=>this.setState({moedaA_valor:event.target.value})}></input>
+                <input type="button"value="Converter" onClick={this.converter}></input>
+                <h2>{this.state.moedaB_valor}</h2>
+            </div>
+        )
+    }
+}
